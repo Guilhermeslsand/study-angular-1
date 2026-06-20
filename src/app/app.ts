8e8fa75ff48156eb52a7adf328978bc9df6b5fd4
Guilherme Sales de Andrade <guilhermeslandrade@gmail.com>
2026-06-20 00:16:17 -0300

    Edição de comportamento geral do app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cabecalho } from "./componentes/cabecalho/cabecalho";
+import { Rodape } from "./componentes/rodape/rodape";
+import { Livro } from "./componentes/livro/livro";
+import { ListaLivros } from "./componentes/lista-livros/lista-livros";
+import { GeneroLiterario } from "./componentes/genero-literario/genero-literario";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Cabecalho, Rodape, ListaLivros, GeneroLiterario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
